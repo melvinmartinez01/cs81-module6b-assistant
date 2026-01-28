2a49f2f169017ce2b0b3fd4df2ba1d8d365d3bbd
Melvin Martinez <melvin.martinez25@icloud.com>
2026-01-28 14:15:16 -0800

    Update assistant.js
    Added comments and methods

--- assistant.js
+++ assistant.js
@@ -1,15 +1,28 @@
+//PersonalAssistant constructor 
 function PersonalAssistant(Wurp) {
   this.name = Wurp;
   this.tasks = [];
   this.mode = "nurturing";
   this.mood = "endearing";
+  this.isFullyActive = false;
+  this.greetTimer = null;
 }
 
+//Method: "Wake Up" trigger
+Personal.Assistant.prototype.startWakeRoutine = function () {
+  console.log("Morning Melvin. I shall slowly open the blinds in your room.")
+  this.greetTimer = setTimeout(() => {
+      this.greetWhenGettingUp();
+  }, 60000);
+};
+
+//Method: addTasks
 PersonalAssistant.prototype.addTask = function(task) {
   this.tasks.push(task);
   console.log(`Added to your agenda today: "${task}"`);
 };
 
+//Method: Removes task after being completed
 PersonalAssistant.prototype.completeTask = function() {
   if (this.tasks.length > 0) {
     const completed = this.tasks.shift();
@@ -20,10 +33,15 @@ PersonalAssistant.prototype.completeTask = function() {
   }
 };
 
+/* Method: Report mode - Essentially a person would set the mode the night before so that when you wake up,
+you would be greeted a nurturing and endearing way.*/
 PersonalAssistant.prototype.reportMode = function() {
   console.log(`Current mode: ${this.mode}`);
 };
 
+
+//Method: This would accompany the mode, with this being endearing to set the tone.
 PersonalAssistant.prototype.reportMood = function() {
   console.log(`Current mood: ${this.mood}`);
 };
+
